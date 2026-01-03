5999b3556099907324a6ac185bad7a1e70c8c18e
kash2104 <kparikh1104@gmail.com>
2026-01-03 18:57:19 +0530

    singleton pattern.

---
+++ singleton/Main.java
@@ -0,0 +1,25 @@
+
+
+class Singleton{
+  private static Singleton instance = null;
+  private Singleton(){
+    System.out.println("constructor called!! Object created");
+  }
+  public static Singleton getInstance(){
+    if(instance == null){
+      instance = new Singleton();
+    }
+    return instance;
+  }
+}
+
+
+public class Main{
+  public static void main(String[] args){
+    Singleton s1 = Singleton.getInstance();
+    Singleton s2 = Singleton.getInstance();
+
+    System.out.println(s1 == s2);
+
+  }
+}
